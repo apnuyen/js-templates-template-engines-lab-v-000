a44c3bba482274a5b93b68202bddcef36c9c7dd4
An Nuyen <annuyen@gmail.com>
2018-02-06 18:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function createPost(){
   var postAuthor = document.getElementById('postAuthor').value;
   var post = document.getElementById('postText').value;
 
-  document.getElementByTagName("main")[0].innerHTML += pageTEmplateFn();
+  document.getElementByTag("main")[0].innerHTML += pageTEmplateFn();
 
   var TemplateHTML = postTemplateFn({'title': postTitle, 'body': post, 'poster': postAuthor});
   var postElement = document.getElementById("post")
 
   postElement.innerHTML = templateHTML;
-  postElement.getElementsByTagName("footer")[0].innerHTML = commentsTemplateFn();
+  postElement.getElementsByTag("footer")[0].innerHTML = commentsTemplateFn();
 }
 
 function postComment(){

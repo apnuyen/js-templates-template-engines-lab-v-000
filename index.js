2ccb9b56dc1653d5367653ae2d97e6fbb99a6e88
An Nuyen <annuyen@gmail.com>
2018-02-06 18:52:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ function createPost(){
   var pageTemplateFn=_.template(document.getElementById("page-template").innerHTML);
   var postTemplateFn=_.template(document.getElementById("post-template").innerHTML);
   var commentsTemplateFn=_.template(document.getElementById("comments-template").innerHTML);
-  var postTitle = document.getElementById("").value;
+  var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById('postAuthor').value;
+  var post = document.getElementById('postText').value;
 }
 
 function postComment(){

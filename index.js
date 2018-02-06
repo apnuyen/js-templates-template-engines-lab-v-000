af5582fb4d0715fc73c09d73f692847be41511f7
An Nuyen <annuyen@gmail.com>
2018-02-06 18:52:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function createPost(){
+  var pageTemplateFn=_.template(document.getElementById("page-template").innerHTML);
+  var postTemplateFn=_.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplateFn=_.template(document.getElementById("comments-template").innerHTML);
+  var postTitle = document.getElementById("").value;
+  var postAuthor = document.getElementById('postAuthor').value;
+}
+
+function postComment(){
+  
+}
